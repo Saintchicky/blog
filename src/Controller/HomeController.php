f15ef52560a503dabd81069dc506ea087b111439
Saintchicky <poussin6494@hotmail.com>
2022-03-29 08:15:06 +0200

    prepa boot

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Article;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function index(): Response
+    {
+        $repo = $this->getDoctrine()->getRepository(Article::class);
+        $articles = $repo->findAll();
+
+        return $this->render('home/index.html.twig',[
+            'articles' => $articles
+        ]);
+    }
+}
